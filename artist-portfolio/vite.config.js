47b34eea3ea0b49f69c017b3c942c92b66488a21
pfolex <alexlpfeil@yahoo.com>
2025-08-03 15:02:55 -0400

    updated the website

--- artist-portfolio/vite.config.js
+++ artist-portfolio/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Replace 'your-repo-name' with the actual repo name on GitHub
 export default defineConfig({
-  base: '/juliapfeilartist/',
   plugins: [react()],
 })
 
