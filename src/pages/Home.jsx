dc508a8ffd87b83e8d44a6d7fa0c82589549c56a
pfolex <alexlpfeil@yahoo.com>
2025-08-02 22:28:59 -0400

    added website

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to My Art Portfolio</h2>
+      <p>Explore my artwork, request a commission, or get in touch!</p>
+    </div>
+  );
+}
+
+export default Home;
+
