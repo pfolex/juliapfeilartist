3e9b283bb2d3ec807523b1e41ce583716924b1dd
pfolex <alexlpfeil@yahoo.com>
2025-08-03 15:21:37 -0400

    updated the website

--- artist-portfolio/src/data/artworks.js
+++ artist-portfolio/src/data/artworks.js
@@ -1,309 +1,355 @@
+
+import artwork1 from '/images/artwork/IMG_4035 2.jpg';
+import artwork2 from '/images/artwork/IMG_4035.jpg';
+import artwork3 from '/images/artwork/IMG_4036.jpg';
+import artwork4 from '/images/artwork/IMG_4037.jpg';
+import artwork5 from '/images/artwork/IMG_4038.jpg';
+import artwork6 from '/images/artwork/IMG_4039.jpg';
+import artwork7 from '/images/artwork/IMG_4040.jpg';
+import artwork8 from '/images/artwork/IMG_4041.jpg';
+import artwork9 from '/images/artwork/IMG_4042.jpg';
+import artwork10 from '/images/artwork/IMG_4043.jpg';
+import artwork11 from '/images/artwork/IMG_4044.jpg';
+import artwork12 from '/images/artwork/IMG_4045.jpg';
+import artwork13 from '/images/artwork/IMG_4046.jpg';
+import artwork14 from '/images/artwork/IMG_4047.jpg';
+import artwork15 from '/images/artwork/IMG_4048.jpg';
+import artwork16 from '/images/artwork/IMG_4049.jpg';
+import artwork17 from '/images/artwork/IMG_4050.jpg';
+import artwork18 from '/images/artwork/IMG_4051.jpg';
+import artwork19 from '/images/artwork/IMG_4052.jpg';
+import artwork20 from '/images/artwork/IMG_4053.jpg';
+import artwork21 from '/images/artwork/IMG_4054.jpg';
+import artwork22 from '/images/artwork/IMG_4055.jpg';
+import artwork23 from '/images/artwork/IMG_4056.jpg';
+import artwork24 from '/images/artwork/IMG_4057.jpg';
+import artwork25 from '/images/artwork/IMG_4058.jpg';
+import artwork26 from '/images/artwork/IMG_4059.jpg';
+import artwork27 from '/images/artwork/IMG_4060.jpg';
+import artwork28 from '/images/artwork/IMG_4061.jpg';
+import artwork29 from '/images/artwork/IMG_4062.jpg';
+import artwork30 from '/images/artwork/IMG_4063.jpg';
+import artwork31 from '/images/artwork/IMG_4064.jpg';
+import artwork32 from '/images/artwork/IMG_4065.jpg';
+import artwork33 from '/images/artwork/IMG_4066.jpg';
+import artwork34 from '/images/artwork/IMG_4067.jpg';
+import artwork35 from '/images/artwork/IMG_4068.jpg';
+import artwork36 from '/images/artwork/IMG_4069.jpg';
+import artwork37 from '/images/artwork/IMG_4070.jpg';
+import artwork38 from '/images/artwork/IMG_4072.jpg';
+import artwork39 from '/images/artwork/IMG_4073.jpg';
+import artwork40 from '/images/artwork/IMG_4074.jpg';
+import artwork41 from '/images/artwork/IMG_4075.jpg';
+import artwork42 from '/images/artwork/IMG_4076.jpg';
+import artwork43 from '/images/artwork/IMG_4078.jpg';
+import artwork44 from '/images/artwork/IMG_4079.jpg';
+
 export const artworks = [
   {
     id: 'IMG_4035 2',
     title: 'Artwork 1',
-    image: '/images/artwork/IMG_4035 2.jpg',
+    image: artwork1,
     dimensions: '2102 x 3455',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4035',
     title: 'Artwork 2',
-    image: '/images/artwork/IMG_4035.jpg',
+    image: artwork2,
     dimensions: '2102 x 3455',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4036',
     title: 'Artwork 3',
-    image: '/images/artwork/IMG_4036.jpg',
+    image: artwork3,
     dimensions: '2379 x 3065',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4037',
     title: 'Artwork 4',
-    image: '/images/artwork/IMG_4037.jpg',
+    image: artwork4,
     dimensions: '2453 x 3420',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4038',
     title: 'Artwork 5',
-    image: '/images/artwork/IMG_4038.jpg',
+    image: artwork5,
     dimensions: '2401 x 2861',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4039',
     title: 'Artwork 6',
-    image: '/images/artwork/IMG_4039.jpg',
+    image: artwork6,
     dimensions: '1955 x 2401',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4040',
     title: 'Artwork 7',
-    image: '/images/artwork/IMG_4040.jpg',
+    image: artwork7,
     dimensions: '2247 x 1715',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4041',
     title: 'Artwork 8',
-    image: '/images/artwork/IMG_4041.jpg',
+    image: artwork8,
     dimensions: '1624 x 3753',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4042',
     title: 'Artwork 9',
-    image: '/images/artwork/IMG_4042.jpg',
+    image: artwork9,
     dimensions: '1577 x 3138',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4043',
     title: 'Artwork 10',
-    image: '/images/artwork/IMG_4043.jpg',
+    image: artwork10,
     dimensions: '1488 x 2591',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4044',
     title: 'Artwork 11',
-    image: '/images/artwork/IMG_4044.jpg',
+    image: artwork11,
     dimensions: '2011 x 1916',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4045',
     title: 'Artwork 12',
-    image: '/images/artwork/IMG_4045.jpg',
+    image: artwork12,
     dimensions: '3318 x 2296',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4046',
     title: 'Artwork 13',
-    image: '/images/artwork/IMG_4046.jpg',
+    image: artwork13,
     dimensions: '2621 x 2057',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4047',
     title: 'Artwork 14',
-    image: '/images/artwork/IMG_4047.jpg',
+    image: artwork14,
     dimensions: '3507 x 2627',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4048',
     title: 'Artwork 15',
-    image: '/images/artwork/IMG_4048.jpg',
+    image: artwork15,
     dimensions: '2229 x 1828',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4049',
     title: 'Artwork 16',
-    image: '/images/artwork/IMG_4049.jpg',
+    image: artwork16,
     dimensions: '2448 x 2716',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4050',
     title: 'Artwork 17',
-    image: '/images/artwork/IMG_4050.jpg',
+    image: artwork17,
     dimensions: '2922 x 2354',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4051',
     title: 'Artwork 18',
-    image: '/images/artwork/IMG_4051.jpg',
+    image: artwork18,
     dimensions: '3028 x 2601',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4052',
     title: 'Artwork 19',
-    image: '/images/artwork/IMG_4052.jpg',
+    image: artwork19,
     dimensions: '2270 x 2346',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4053',
     title: 'Artwork 20',
-    image: '/images/artwork/IMG_4053.jpg',
+    image: artwork20,
     dimensions: '2482 x 2882',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4054',
     title: 'Artwork 21',
-    image: '/images/artwork/IMG_4054.jpg',
+    image: artwork21,
     dimensions: '2545 x 2485',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4055',
     title: 'Artwork 22',
-    image: '/images/artwork/IMG_4055.jpg',
+    image: artwork22,
     dimensions: '3539 x 2802',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4056',
     title: 'Artwork 23',
-    image: '/images/artwork/IMG_4056.jpg',
+    image: artwork23,
     dimensions: '2193 x 2697',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4057',
     title: 'Artwork 24',
-    image: '/images/artwork/IMG_4057.jpg',
+    image: artwork24,
     dimensions: '943 x 1365',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4058',
     title: 'Artwork 25',
-    image: '/images/artwork/IMG_4058.jpg',
+    image: artwork25,
     dimensions: '2226 x 2264',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4059',
     title: 'Artwork 26',
-    image: '/images/artwork/IMG_4059.jpg',
+    image: artwork26,
     dimensions: '1706 x 1722',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4060',
     title: 'Artwork 27',
-    image: '/images/artwork/IMG_4060.jpg',
+    image: artwork27,
     dimensions: '1796 x 2274',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4061',
     title: 'Artwork 28',
-    image: '/images/artwork/IMG_4061.jpg',
+    image: artwork28,
     dimensions: '1719 x 2261',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4062',
     title: 'Artwork 29',
-    image: '/images/artwork/IMG_4062.jpg',
+    image: artwork29,
     dimensions: '2309 x 2902',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4063',
     title: 'Artwork 30',
-    image: '/images/artwork/IMG_4063.jpg',
+    image: artwork30,
     dimensions: '2447 x 1777',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4064',
     title: 'Artwork 31',
-    image: '/images/artwork/IMG_4064.jpg',
+    image: artwork31,
     dimensions: '1958 x 2070',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4065',
     title: 'Artwork 32',
-    image: '/images/artwork/IMG_4065.jpg',
+    image: artwork32,
     dimensions: '1583 x 1526',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4066',
     title: 'Artwork 33',
-    image: '/images/artwork/IMG_4066.jpg',
+    image: artwork33,
     dimensions: '2316 x 2869',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4067',
     title: 'Artwork 34',
-    image: '/images/artwork/IMG_4067.jpg',
+    image: artwork34,
     dimensions: '1561 x 1760',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4068',
     title: 'Artwork 35',
-    image: '/images/artwork/IMG_4068.jpg',
+    image: artwork35,
     dimensions: '2792 x 2031',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4069',
     title: 'Artwork 36',
-    image: '/images/artwork/IMG_4069.jpg',
+    image: artwork36,
     dimensions: '2125 x 2703',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4070',
     title: 'Artwork 37',
-    image: '/images/artwork/IMG_4070.jpg',
+    image: artwork37,
     dimensions: '1905 x 2425',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4072',
     title: 'Artwork 38',
-    image: '/images/artwork/IMG_4072.jpg',
+    image: artwork38,
     dimensions: '1795 x 2400',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4073',
     title: 'Artwork 39',
-    image: '/images/artwork/IMG_4073.jpg',
+    image: artwork39,
     dimensions: '2396 x 1886',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4074',
     title: 'Artwork 40',
-    image: '/images/artwork/IMG_4074.jpg',
+    image: artwork40,
     dimensions: '1520 x 1723',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4075',
     title: 'Artwork 41',
-    image: '/images/artwork/IMG_4075.jpg',
+    image: artwork41,
     dimensions: '3093 x 2492',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4076',
     title: 'Artwork 42',
-    image: '/images/artwork/IMG_4076.jpg',
+    image: artwork42,
     dimensions: '2737 x 2191',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4078',
     title: 'Artwork 43',
-    image: '/images/artwork/IMG_4078.jpg',
+    image: artwork43,
     dimensions: '2378 x 1970',
     description: 'This is a beautiful piece of art.'
   },
   {
     id: 'IMG_4079',
     title: 'Artwork 44',
-    image: '/images/artwork/IMG_4079.jpg',
+    image: artwork44,
     dimensions: '2518 x 2124',
     description: 'This is a beautiful piece of art.'
   }
