5fff9c07b29c69f0890fcb6518d3c58a10eab2fd
pfolex <alexlpfeil@yahoo.com>
2025-08-07 15:24:25 -0400

    Debug: Add console logs to Artwork.jsx for ID and artwork data

--- artist-portfolio/src/pages/Artwork.jsx
+++ artist-portfolio/src/pages/Artwork.jsx
@@ -4,7 +4,9 @@ import artworkData from '../data/artworkData';
 
 function Artwork() {
   const { id } = useParams();
+  console.log('Artwork ID from URL:', id);
   const artwork = artworkData.find(item => item.id === id);
+  console.log('Found Artwork object:', artwork);
 
   if (!artwork) {
     return <div>Artwork not found.</div>;
